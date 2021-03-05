ded0606a19d9e0f3439f2f014cab0c2da200d357
DESKTOP-76V6K1K\se5ng <c9604100@gmail.com>
2021-03-05 22:08:45 +0900

    Partials and Local Variables

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
-export const join = (req, res) => res.send("Join");
-export const login = (req, res) => res.send("Login");
-export const logout = (req, res) => res.send("Logout");
+export const join = (req, res) => res.render("join");
+export const login = (req, res) => res.render("login");
+export const logout = (req, res) => res.render("logout");
 export const users = (req, res) => res.send("Users");
 export const userDetail = (req, res) => res.send("User Detail");
 export const editProfile = (req, res) => res.send("Edit Profile");
