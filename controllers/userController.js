58540c495ec3ca412af56a4d9240308fd9f33b87
DESKTOP-76V6K1K\se5ng <c9604100@gmail.com>
2021-03-10 10:13:05 +0900

    2.18 ~ 2.22 commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,8 +3,8 @@ export const login = (req, res) => res.render("login", { pageTitle: "Login" });
 export const logout = (req, res) =>
   res.render("logout", { pageTitle: "Logout" });
 export const userDetail = (req, res) =>
-  res.send("userDetail", { pageTitle: "User Detail" });
+  res.render("userDetail", { pageTitle: "User Detail" });
 export const editProfile = (req, res) =>
-  res.send("editProfile", { pageTitle: "Edit Profile" });
+  res.render("editProfile", { pageTitle: "Edit Profile" });
 export const changePassword = (req, res) =>
-  res.send("changePassword", { pageTitle: "Change Password" });
+  res.render("changePassword", { pageTitle: "Change Password" });
---
+++ db.js
@@ -0,0 +1,54 @@
+export const videos = [
+  {
+    id: 242,
+    title: "Video 1",
+    description: "this is slsls",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "seonghee",
+      email: "hhs@hsk.com",
+    },
+  },
+  {
+    id: 2444,
+    title: "Video 2",
+    description: "this is slsls",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "seonghee",
+      email: "hhs@hsk.com",
+    },
+  },
+  {
+    id: 22222,
+    title: "Video 3",
+    description: "this is slsls",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "seonghee",
+      email: "hhs@hsk.com",
+    },
+  },
+  {
+    id: 2222542,
+    title: "Video 4",
+    description: "this is slsls",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 121212,
+      name: "seonghee",
+      email: "hhs@hsk.com",
+    },
+  },
+];
