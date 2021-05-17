7923c3daf6519910c9b8af5cfa9c27b79115799e
DESKTOP-76V6K1K\se5ng <c9604100@gmail.com>
2021-05-17 22:08:59 +0900

    11.5 Building for Production

--- src/init.js
+++ src/init.js
@@ -1,11 +1,13 @@
+import "@babel/polyfill";
 import dotenv from "dotenv";
 import "./db";
 import app from "./app";
+
 dotenv.config();
 
-import "./models/Video";
-import "./models/Comment";
-import "./models/User";
+// import "./models/Video";
+// import "./models/Comment";
+// import "./models/User";
 
 const PORT = process.env.PORT || 4000;
 
